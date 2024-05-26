96e0159a5dd9042b1ad6941bf8c8ef1d19d051bc
ja-shuvro <jashuvro8199gmail.com>
2024-05-26 23:23:48 +0600

    feat add FAQ Authentication page

---
+++ components/navBar.js
@@ -0,0 +1,306 @@
+document.getElementById("nav-bar").innerHTML = `
+
+<div
+        data-collapse="medium"
+        data-animation="default"
+        data-duration="400"
+        data-easing="ease"
+        data-easing2="ease"
+        role="banner"
+        class="navigation w-nav"
+      >
+        <div class="nav-container">
+          <div class="nav-wrapper">
+            <a
+              href="index.html"
+              aria-current="page"
+              class="nav-logo-link w-inline-block w--current"
+              ><div class="logo_svg w-embed">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="auto"
+                  height="auto"
+                  viewBox="0 0 1024 325"
+                  fill="none"
+                >
+                  <path
+                    d="M149.096 111.735L205.581 151.683L141.942 202.266C164.696 181.509 120.928 156.894 149.096 111.735Z"
+                    fill="#00A4DD"
+                  />
+                  <path
+                    d="M110.218 324.828L255.276 189.724C258.469 186.751 258.02 181.572 254.368 179.196L149.267 111.852C181.877 158.886 179.346 164.129 146.491 197.63C115.974 228.745 114.788 229.288 102.957 242.608C69.9843 279.701 110.218 324.828 110.218 324.828Z"
+                    fill="url(#paint0_linear_430_487)"
+                  />
+                  <path
+                    d="M108.319 213.094L51.833 173.146L115.473 122.563C92.7289 143.331 136.497 167.935 108.319 213.094Z"
+                    fill="#FFBE08"
+                  />
+                  <path
+                    d="M147.196 0L2.138 135.104C-1.05466 138.077 -0.60619 143.256 3.04561 145.632L106.396 213.083C73.786 166.048 78.0571 160.699 110.923 127.198C141.44 96.0828 142.626 95.5393 154.457 82.2197C187.44 45.127 147.196 0 147.196 0Z"
+                    fill="url(#paint1_linear_430_487)"
+                  />
+                  <path
+                    d="M295.554 185.414L307.535 171.136C315.191 177.22 323.733 180.438 332.713 180.438C338.479 180.438 341.586 178.446 341.586 175.121V174.897C341.586 171.69 339.034 169.921 328.495 167.481C311.966 163.719 299.217 159.073 299.217 143.132V142.909C299.217 128.523 310.642 118.113 329.275 118.113C342.472 118.113 352.787 121.651 361.223 128.406L350.459 143.569C343.359 138.593 335.596 135.929 328.719 135.929C323.509 135.929 320.957 138.146 320.957 140.905V141.129C320.957 144.667 323.615 146.223 334.379 148.652C352.232 152.531 363.326 158.285 363.326 172.787V173.011C363.326 188.846 350.79 198.255 331.933 198.255C318.18 198.255 305.089 193.939 295.554 185.414Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M373.747 119.433H396.491L415.017 149.429L433.543 119.433H456.286V196.921H434.877V152.423L415.027 182.749H414.579L394.836 152.636V196.91H373.758V119.433H373.747Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M496.862 118.878H517.609L550.668 196.921H527.593L521.934 183.079H491.982L486.441 196.921H463.814L496.862 118.878ZM515.719 166.371L507.07 144.345L498.304 166.371H515.719Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M558.195 119.433H594.906C606.779 119.433 614.98 122.533 620.201 127.851C624.75 132.283 627.078 138.251 627.078 145.891V146.115C627.078 157.964 620.757 165.817 611.104 169.909L629.63 196.921H604.783L589.14 173.457H588.915H579.711V196.921H558.195V119.433V119.433ZM593.913 156.632C601.238 156.632 605.445 153.094 605.445 147.447V147.223C605.445 141.138 601.003 138.038 593.795 138.038H579.711V156.632H593.913Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M656.912 138.251H633.624V119.433H701.726V138.251H678.428V196.921H656.912V138.251Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M743.316 172.019H727.898V196.921H714.251V119.433H744.865C762.942 119.433 774.367 129.62 774.367 145.443C774.378 162.94 760.401 172.019 743.316 172.019ZM743.765 131.836H727.898V159.733H743.765C754.08 159.733 760.518 154.085 760.518 145.784C760.518 136.588 753.973 131.836 743.765 131.836Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M838.156 196.921L819.075 169.909H801.99V196.921H788.344V119.433H822.951C840.804 119.433 851.791 128.842 851.791 144.005C851.791 156.845 844.135 164.592 833.266 167.586L854.226 196.921H838.156V196.921ZM821.851 131.836H801.99V157.847H821.958C831.717 157.847 837.932 152.753 837.932 144.783C837.932 136.258 831.942 131.836 821.851 131.836Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M905.587 198.255C881.743 198.255 865.097 180.214 865.097 158.296C865.097 136.377 881.957 118.113 905.8 118.113C929.644 118.113 946.291 136.153 946.291 158.072C946.291 179.991 929.441 198.255 905.587 198.255ZM905.587 130.612C890.168 130.612 879.405 142.898 879.405 158.061C879.405 173.224 890.392 185.734 905.8 185.734C921.209 185.734 931.982 173.448 931.982 158.285C931.993 143.122 921.006 130.612 905.587 130.612Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M991.884 172.019H976.466V196.921H962.819V119.433H993.433C1011.51 119.433 1022.94 129.62 1022.94 145.443C1022.94 162.94 1008.97 172.019 991.884 172.019ZM992.322 131.836H976.466V159.733H992.322C1002.64 159.733 1009.08 154.085 1009.08 145.784C1009.08 136.588 1002.53 131.836 992.322 131.836Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M324.019 252.808H317.559V222.333H305.984V216.408H335.594V222.333H324.019V252.808V252.808Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M376.191 252.812L367.222 240.121H359.192V252.812H352.785V216.401H369.047C377.44 216.401 382.598 220.823 382.598 227.952C382.598 233.983 378.999 237.628 373.895 239.034L383.751 252.823H376.191V252.812ZM368.524 222.23H359.192V234.452H368.578C373.158 234.452 376.084 232.055 376.084 228.314C376.084 224.308 373.265 222.23 368.524 222.23Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M435.805 252.81H429.025L425.32 244.019H408.064L404.316 252.81H397.75L413.798 216.144H419.735L435.805 252.81ZM416.681 223.784L410.424 238.351H422.981L416.681 223.784Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M466.087 252.812H452.483V216.401H466.087C477.555 216.401 485.478 224.255 485.478 234.559C485.478 244.852 477.555 252.812 466.087 252.812ZM466.087 222.23H458.89V246.994H466.087C473.743 246.994 478.751 241.847 478.751 234.665C478.751 227.483 473.743 222.23 466.087 222.23Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M530.708 222.123H510.068V231.586H528.359V237.308H510.068V247.09H530.965V252.812H503.651V216.401H530.698V222.123H530.708Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M572.395 252.812L563.426 240.121H555.396V252.812H548.989V216.401H565.251C573.644 216.401 578.802 220.823 578.802 227.952C578.802 233.983 575.203 237.628 570.099 239.034L579.955 252.823H572.395V252.812ZM564.739 222.23H555.407V234.452H564.792C569.373 234.452 572.299 232.055 572.299 228.314C572.299 224.308 569.48 222.23 564.739 222.23Z"
+                    fill="currentColor"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="paint0_linear_430_487"
+                      x1="89.9711"
+                      y1="218.339"
+                      x2="257.416"
+                      y2="218.339"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#153E6C" />
+                      <stop offset="1" stop-color="#0080B7" />
+                    </linearGradient>
+                    <linearGradient
+                      id="paint1_linear_430_487"
+                      x1="0.00415368"
+                      y1="106.543"
+                      x2="167.449"
+                      y2="106.543"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stop-color="#C5161E" />
+                      <stop offset="1" stop-color="#FBC024" />
+                    </linearGradient>
+                  </defs>
+                </svg></div
+            ></a>
+            <nav role="navigation" class="nav-menu w-nav-menu">
+              <a href="affiliate-program.html" class="nav-link w-nav-link"
+                >Affiliate Program</a
+              ><a
+                href="https://help.smartproptrader.com/en"
+                target="_blank"
+                class="nav-link w-nav-link"
+                >FAQs</a
+              ><a href="about.html" class="nav-link w-nav-link">About</a
+              ><a href="charity.html" class="nav-link w-nav-link">Giving Back</a
+              ><a href="payouts.html" class="nav-link w-nav-link">Payouts</a
+              ><a href="blog.html" class="nav-link w-nav-link">Blog</a
+              ><a href="testimonials.html" class="nav-link w-nav-link"
+                >Testimonials</a
+              ><a href="contact-us.html" class="nav-link w-nav-link"
+                >Contact Us</a
+              ><a
+                href="https://trader.smartproptrader.com/account/login/"
+                class="nav-link responsive w-nav-link"
+                >Login</a
+              >
+            </nav>
+          </div>
+          <div class="nav-button-group">
+            <div
+              data-hover="true"
+              data-delay="0"
+              class="language-dropdown w-dropdown"
+            >
+              <div class="language-toggle new w-dropdown-toggle">
+                <div class="flag-1 new"></div>
+              </div>
+              <nav class="language-list new w-dropdown-list">
+                <a
+                  data-lang="en"
+                  href="index.html#googtrans(en|en)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-1"></div>
+                  <div>English</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="nl"
+                  href="index.html#googtrans(en|nl)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-2"></div>
+                  <div>Dutch</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="es"
+                  href="index.html#googtrans(en|es)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-3"></div>
+                  <div>Spanish</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="fr"
+                  href="index.html#googtrans(en|fr)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-4"></div>
+                  <div>French</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="de"
+                  href="index.html#googtrans(en|de)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-4 _5"></div>
+                  <div>German</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="sv"
+                  href="index.html#googtrans(en|sv)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-4 _6"></div>
+                  <div>Swedish</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="zh-CN"
+                  href="index.html#googtrans(en|zh-CN)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-4 _7"></div>
+                  <div>Chinese</div>
+                  <div class="langauge-line-base">
+                    <div class="langauge-line-break"></div></div></a
+                ><a
+                  data-lang="pt"
+                  href="index.html#googtrans(en|pt)"
+                  class="lang-select new w-inline-block"
+                  ><div class="flag-4 _8"></div>
+                  <div>Portuguese</div></a
+                >
+              </nav>
+            </div>
+            <a
+              tr-color-toggle=""
+              href="index.html#"
+              class="toggle_wrap w-inline-block"
+              ><div
+                data-w-id="7193df9e-8d18-5363-b7cd-a01f00b3d46a"
+                class="toggle"
+              >
+                <div class="toggle-circle">
+                  <div
+                    data-is-ix2-target="1"
+                    class="toggle-lottie"
+                    data-w-id="7193df9e-8d18-5363-b7cd-a01f00b3d46c"
+                    data-animation-type="lottie"
+                    data-src="https://assets-global.website-files.com/65da995558b734286abb78f8/65da995558b734286abb7950_lf20_cvalozqq.json"
+                    data-loop="0"
+                    data-direction="1"
+                    data-autoplay="0"
+                    data-renderer="svg"
+                    data-default-duration="0.5338671787890117"
+                    data-duration="0"
+                    data-ix2-initial-state="100"
+                  ></div>
+                </div></div
+            ></a>
+            <div class="nav-menu-button w-nav-button">
+              <div class="icon-5 w-icon-nav-menu"></div>
+            </div>
+            <div class="nav-button-wrap">
+              <a
+                href="https://trader.smartproptrader.com/account/login/"
+                target="_blank"
+                class="button-outline-m w-inline-block"
+                ><div class="icon-regular-2 w-embed">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="CurrentColor"
+                  >
+                    <path
+                      d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z"
+                      fill="CurrentColor"
+                    />
+                  </svg>
+                </div>
+                <div>Login</div></a
+              ><a
+                href="https://trader.smartproptrader.com/account/register/"
+                target="_blank"
+                class="button-1 w-inline-block"
+                ><div class="button-1">
+                  <div class="button-1_color-wrap">
+                    <div class="button-1_color _1"></div>
+                    <div class="button-1_color _3"></div>
+                    <div class="button-1_color _2"></div>
+                    <div class="label">Get Started</div>
+                    <div class="icon-regular-2 w-embed">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path
+                          d="M12.879 12L9.1665 8.28749L10.227 7.22699L15 12L10.227 16.773L9.1665 15.7125L12.879 12Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </div>
+                    <div class="button-1_bg"></div>
+                  </div></div
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+`;
